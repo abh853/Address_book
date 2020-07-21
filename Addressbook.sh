f8b4ef2e1e44dadb72a3772198bdc26923ee399f
abhishek <abhishek96890@gmail.com>
2020-07-21 22:33:48 +0530

    ADDED :bookclass

--- Addressbook.sh
+++ Addressbook.sh
@@ -1,3 +1,57 @@
-#!/bin/bash -x
+class Address
+{
+  private String address;
+  private String city;
+  private String state;
+  private int zip;
+  
+  Address(){}
+  Address(  String address,
+  	    String city,
+  	    String state,
+  	    int zip)
+    {
+	this.address = address;
+	this.city = city;
+	this.state = state;
+	this.zip  =  zip;
+    }
+   
+    public String getAddress()
+    {
+	return address;
+    }
 
-echo "briedgelabz is very good institude"
+    public String getState() 
+    {
+	return state;
+    }
+    public String getCity() 
+    {
+	return city;
+    }
+    public int getZip() 
+    {
+	return zip;
+    }
+
+
+    public void setAddress(String address)
+    {
+        this.address =  address;
+    }
+
+    public void setState(String state) 
+    {
+        this.state = state;
+    }
+    public void setCity(String city) 
+    {
+        this.city = city;
+    }
+    public void setZip(int zip) 
+    {
+        this.zip = zip;
+    }
+
+}
